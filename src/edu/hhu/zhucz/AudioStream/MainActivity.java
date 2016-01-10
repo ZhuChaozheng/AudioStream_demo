abd12d33a17653532ce43628d60738b063886e4c
ZhuChaozheng <370045744@qq.com>
2016-01-10 09:57:29 +0800

    first upload

---
+++ src/edu/hhu/zhucz/AudioStream/MainActivity.java
@@ -0,0 +1,71 @@
+package edu.hhu.zhucz.AudioStream;
+
+import com.example.heuy.R;
+
+import edu.hhu.zhucz.AudioStream.VolPlayer;
+import edu.hhu.zhucz.AudioStream.VolRecorder;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+	public static final int mi1 = Menu.FIRST ;
+    public static final int mi2 = Menu.FIRST + 1 ;
+    public static final int mi3 = Menu.FIRST + 2 ;
+ 
+    protected VolPlayer     m_player ;
+    protected VolRecorder   m_recorder ;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch(item.getItemId()) {
+	    case R.id.mi1:
+        {
+         m_player = new VolPlayer() ;
+            m_recorder = new VolRecorder() ;
+            m_recorder.init() ;
+            m_player.init() ;
+            m_recorder.start() ;
+            m_player.start() ;
+           
+        }
+        break ;
+    case R.id.mi2:
+        {  
+         m_recorder.free() ;
+            m_player.free() ;
+
+            m_player = null ;
+            m_recorder = null ;
+        }
+        break ;
+    case R.id.mi3:
+        {
+           int pid = android.os.Process.myPid() ;
+            android.os.Process.killProcess(pid) ;
+           //Toast.makeText(getApplicationContext(), "ÍË³ö?", Toast.LENGTH_LONG).show();
+        }
+        break ;
+    default:
+        break ;
+    }
+
+    return super.onOptionsItemSelected(item);
+}
+
+}
---
+++ src/edu/hhu/zhucz/AudioStream/VolRecorder.java
@@ -0,0 +1,83 @@
+package edu.hhu.zhucz.AudioStream;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.LinkedList;
+
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.util.Log;
+
+public class VolRecorder extends Thread
+{
+ 
+    protected AudioRecord record ;
+    protected int         in_buf_size ;
+    protected byte []     in_bytes ;
+    protected boolean     keep_running ;
+    protected Socket      s;
+    protected DataOutputStream dout;
+    protected LinkedList<byte[]>  m_in_q ;
+    protected ServerSocket ss;
+    public void init()
+    {
+     in_buf_size =  AudioRecord.getMinBufferSize(8000,
+                        AudioFormat.CHANNEL_IN_MONO,
+                        AudioFormat.ENCODING_PCM_16BIT);
+  
+  record = new AudioRecord(MediaRecorder.AudioSource.MIC,
+  8000,
+  AudioFormat.CHANNEL_IN_MONO,
+  AudioFormat.ENCODING_PCM_16BIT, in_buf_size) ;
+ 
+  in_bytes = new byte [in_buf_size] ;
+ 
+  keep_running = true ;
+  m_in_q=new LinkedList<byte[]>();
+    }
+  
+    public void run ()
+ {
+      try
+      {
+    	  ss = new ServerSocket(5060);
+     	 s = ss.accept();
+     	 dout=new DataOutputStream(s.getOutputStream());
+          byte [] bytes_pkg ;		//用于存储录制好的语音
+             record.startRecording() ;  //开始录音
+             while(keep_running)
+             {
+                 record.read(in_bytes, 0, in_buf_size) ;
+                 bytes_pkg = in_bytes.clone() ;
+                 if(m_in_q.size() >= 2)
+                 {
+                    dout.write(m_in_q.removeFirst() , 0, m_in_q.removeFirst() .length);
+                 }
+                    m_in_q.add(bytes_pkg) ;
+             }
+             record.stop() ;
+             record = null ;
+             in_bytes = null ;
+       dout.close();
+     
+      }
+      catch(Exception e)
+      {
+       e.printStackTrace();
+      }
+    }
+    public void free()
+ {
+  keep_running = false ;
+        try {
+            Thread.sleep(1000) ;
+        } catch(Exception e) {
+            Log.d("sleep exceptions...\n","") ;
+        }
+ }
+}
+
